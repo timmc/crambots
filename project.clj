9b7c8ae4c4bf7dde17e54fd699cccc7ad538878b
Tim McCormack <cortex@brainonfire.net>
2012-05-31 15:06:01 -0400

    Add url and license to project

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject crambots "1.0.0-SNAPSHOT"
   :description "TimMc's crosscram bots"
+  :url "https://github.com/timmc/crambots"
+  :license {:name "Eclipse Public License v1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.timmc/handy "1.2.0"]]
   :dev-dependencies [[lein-git-deps "0.0.1-SNAPSHOT"]]
